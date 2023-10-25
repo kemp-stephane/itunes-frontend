756e8fb54ad06905cab3448cb92d67393ba9e8bb
kemp-stephane <114095693+kemp-stephane@users.noreply.github.com>
2023-10-25 18:27:46 +0200

    adding file

---
+++ frontend/src/components/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import App from '../App';
+
+import { render } from '@testing-library/react';
+
+test("renders correctly", () => { 
+   const {container} = render(<App/>);
+   expect(container.innerHTML).toMatchSnapshot();
+});
---
+++ frontend/src/components/Basket.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import AddFav from './AddFav';
+import './page.css'
+
+const Basket = ({fetchFavourites, favourites}) => {
+   return (
+      <div className="favo">
+         <div> 
+            <Link to='/'>  {/* this button link you back to the homepage*/}
+               <button className="back">Back</button>
+            </Link>
+               </div>
+               <h3>
+                  <u>My Favourites</u>
+               </h3>
+            <br/><br/>   
+
+         <div>
+            {(favourites === undefined) ? (
+               <div>
+               </div>
+
+            ) : (
+
+               <div> {/* what you will see inside the basket */}
+                  {favourites && favourites.map((artists) => (
+                     <AddFav
+                        artists={artists}
+                        key={artists.id}
+                        fetchFavourites={fetchFavourites}
+                     />
+                  ))}
+               </div>
+            )}
+         </div>
+      </div>
+   )
+}
+
+export default Basket
