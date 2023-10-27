a2f63d9f1ddbb91a586c409aa7c1c1dff10a6b4a
kemp-stephane <114095693+kemp-stephane@users.noreply.github.com>
2023-10-27 14:07:49 +0200

    adding file

---
+++ frontend/src/components/AddFav.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import './app.css'
+
+
+const AddFav = ({ artists,fetchFavourites }) => {
+   
+   const removeItem = async () => {
+      await fetch(`/api/delete/${artists.id}`, {
+         method: 'DELETE'
+      });
+      fetchFavourites();
+   }
+
+
+   const handleRemove = (e) => {
+      e.preventDefault();// 
+      removeItem();
+   }
+   return (
+      <div>
+         <div>
+            {artists.favItem.artworkUrl100 ? (
+               <img src={artists.favItem.artworkUrl100} alt='media artwork' />
+            ) : (
+               <div>
+                  No image
+               </div>
+            )}
+         </div>
+         <div>
+            {!artists.favItem.trackName ? (
+               <div>
+                  <span>Name:</span> {artists.favItem.collectionName}
+               </div>
+            ) : (
+               <div>
+                  <span>Name:</span> {artists.favItem.trackName}
+               </div>
+            )}
+            <div>
+               <span>Artist:</span> {artists.favItem.artistName}
+            </div>
+            {artists.favItem.kind ? (
+               <div>
+                  <span>Type:</span> {artists.favItem.kind}
+               </div>
+            ) : (
+               <div>
+                  <span>Type:</span> {artists.favItem.wrapperType}
+               </div>
+            )}
+         </div>
+         {/*this button allow users to remove item from favourites */}
+         <button onClick={handleRemove}>Remove</button>
+         <div>
+         ------------------------------------------
+            <br/><br/>
+         </div>
+      </div>
+   )
+}
+
+export default AddFav
