c8eda3fdc5e9ec3150eb5c8677891b1753292683
kemp-stephane <114095693+kemp-stephane@users.noreply.github.com>
2023-10-27 14:05:50 +0200

    Delete frontend directory

--- frontend/src/components/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import App from '../App';
-
-import { render } from '@testing-library/react';
-
-test("renders correctly", () => { 
-   const {container} = render(<App/>);
-   expect(container.innerHTML).toMatchSnapshot();
-});
